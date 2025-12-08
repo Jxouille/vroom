3be3b5c2bdba6ba0af3990c29a999f48b9448fb5
Jxouille <steinhart.axel@gmail.com>
2025-12-08 12:00:21 +0100

    début de la page détail de trajet

--- Vue/pages/v_detail_trajet.php
+++ Vue/pages/v_detail_trajet.php
@@ -1,7 +1,44 @@
 <?php require "../head_&_header.php";?>
 
 <body>
-    ... Contenu de la page ...
+    <div class="detail-trajet">
+        <section class="header-trajet">
+            <img src="../../Ressources/Image/FeuilleLogin2.png" alt="" class="icone">
+            <h2>Détails du Trajet</h2>
+            <h3>Voici les informations complètes de votre trajet écologique</h3><br> 
+        </section>
+
+        <div class="info-trajet">
+            <h3>Itinéraire :</h3>
+            <div class="itineraire-detailed">
+                <div class="stop-row departure">
+                    <div class="dot green"></div>
+                    <div class="stop-info">
+                        <p class="stop-type">Départ</p>
+                        <p class="time">10:00</p>
+                    </div>
+                </div>
+
+                <div class="connector">
+                    <div class="vertical-line"></div>
+                    <div class="duration">Durée : 2h 15m</div>
+                </div>
+
+                <div class="stop-row arrival">
+                    <div class="dot green"></div>
+                    <div class="stop-info">
+                        <p class="stop-type">Arrivée</p>
+                        <p class="time">12:15</p>
+                        <p class="address">Lyon — Place Bellecour, 69002</p>
+                    </div>
+                </div>
+            </div>
+            <h3>Date et Heure :</h3>
+            <p>15 août 2024, 10:00 AM</p>
+            <h3>Conducteur :</h3>
+            <p>Nom : Jean Dupont</p>
+            <p>Contact :
+    </div>
 </body>
 
 <?php include "../footer.php";?>
