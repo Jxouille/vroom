af67cd2ea32a5ae09e74036e79162b036ab42073
myab08 <myanhbui08112004@gmail.com>
2025-12-09 15:22:32 +0100

    j'en ai marre

--- Vue/head_&_header.php
+++ Vue/head_&_header.php
@@ -12,16 +12,22 @@ $css_chemin  = "../../Ressources/CSS/" . $nom_ce_fichier . ".css";
     <link rel="stylesheet" href="<?php echo $css_chemin; ?>">
 </head>
 <header>
-    <nav>
-        <img src="../../Ressources/Image/vroom_logo_sans_fond.png"  alt="Logo Vroom">
+    <img src="../../Ressources/Image/logo_ver2.png"  alt="Logo Vroom">
+    <nav class="nav-main">
         <a href="v_accueil.php">Accueil</a>
         <a href="v_recherche_trajet.php">Réserver</a>
         <a href="v_publie_trajet.php">Publier</a>
-        <a href="v_mes_trajets.php">Mes trajets</a>
-        <a href="v_messagerie.php">Messagerie</a>
-        <a href="v_profile.php">Profil</a>
-        <a href="v_connection.php" class="button-header">Se connecter</a>
-        <a href="v_inscription.php" class="button-header secondary">S'inscrire</a>
+    </nav>
+    <nav class="nav-profile">
+        <img src="../../Ressources/Image/person_icon.png"  alt="Logo Vroom">
+        <ul>
+            <li><a href="v_mes_trajets.php">Mes trajets"</a></li>
+            <li> <a href="v_messagerie.php">Messagerie</a></li>
+            <li><a href="v_profile.php">Profil</a></li> 
+            <li><a href="v_connection.php" class="button-header">Se connecter</a></li> 
+            <li><a href="v_inscription.php" class="button-header secondary">S'inscrire</a></li> 
+        </ul>
+        
     </nav>
 </header>
 
