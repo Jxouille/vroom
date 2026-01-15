565e6c53b9c48cf46efa4008c0b4949975505796
myab08 <myanhbui08112004@gmail.com>
2026-01-15 18:51:03 +0100

    nav

--- Vue/header.php
+++ Vue/header.php
@@ -6,26 +6,29 @@
             <a href="index.php?page=recherche_trajet">Chercher un trajet </a>
             <a href="index.php?page=publie_trajet">Publier</a>
         </nav>
-        <nav class="nav-profile">
-            <label for="show-menu" class="show-menu">Show Menu</label>
-            <input type="checkbox" id="show-menu" role="button">
-                <img src="../../Ressources/Image/person_icon.png"  alt="Logo Vroom">
-                <ul classe="hidden">
-                    <?php if (isset($_SESSION['user_id'])) { ?>
-                        <li><a href="index.php?page=profil" class="button-header">Mon profil</a></li> 
-                        <li><a href="index.php?page=mes_reservations" class="button-header">Mes réservations</a></li> 
-                        <li><a href="index.php?page=mes_annonces" class="button-header">Mes annonces</a></li> 
-                        <li><a href="index.php?page=mes_paiements" class="button-header">Mes paiements</a></li> 
-                        <li><a href="index.php?page=mes_documents" class="button-header">Mes documents</a></li> 
-                        <li><a href="index.php?page=favoris" class="button-header">Mes favoris</a></li> 
-                        <li><a href="index.php?page=connexion&action=deconnexion" class="button-header">Se déconnecter</a></li>
-                    <?php } elseif ($page = 'connexion') { ?>
-                        <li><a href="index.php?page=connexion" class="button-header">Se connecter</a></li> 
-                     <?php } else { ?>
-                        <li><a href="index.php?page=connexion" class="button-header">Se connecter</a></li>
-                        <li><a href="index.php?page=inscription" class="button-header">S'inscrire</a></li> 
-                    <?php } ?>
-                </ul>
+        <nav class="nav-profile" aria-label="Menu profil">
+            <?php if (isset($_SESSION['user_id'])): ?>
+                <div class="profile">
+                    <a href="index.php?page=profil" class="profile-link">Mon profil</a>
+                    <ul class="profile-menu" role="menu" aria-hidden="true">
+                        <li role="menuitem"><a href="index.php?page=mes_reservations">Mes réservations</a></li>
+                        <li role="menuitem"><a href="index.php?page=mes_annonces">Mes annonces</a></li>
+                        <li role="menuitem"><a href="index.php?page=mes_paiements">Mes paiements</a></li>
+                        <li role="menuitem"><a href="index.php?page=mes_documents">Mes documents</a></li>
+                        <li role="menuitem"><a href="index.php?page=favoris">Mes favoris</a></li>
+                        <li role="menuitem"><a href="index.php?page=connexion&action=deconnexion">Se déconnecter</a></li>
+                    </ul>
+                </div>
+            <?php else: ?>
+                <div class="profile">
+                    <a href="index.php?page=connexion" class="profile-link">Se connecter</a>
+                    <a href="index.php?page=inscription" class="profile-link profile-signup">S'inscrire</a>
+                    <ul class="profile-menu-auth" role="menu" aria-hidden="true">
+                        <li role="menuitem"><a href="index.php?page=connexion">Se connecter</a></li>
+                        <li role="menuitem"><a href="index.php?page=inscription">S'inscrire</a></li>
+                    </ul>
+                </div>
+            <?php endif; ?>
         </nav>
     </div>
 </header>
