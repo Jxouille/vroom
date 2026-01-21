93652f7c28ff3854c0444763ad29dede4c501b5b
Jxouille <steinhart.axel@gmail.com>
2026-01-21 12:49:11 +0100

    Fix: load CSS from Ressources/Css for case-sensitive deploy servers

--- Vue/head.php
+++ Vue/head.php
@@ -7,13 +7,13 @@
     <title>VROOM - <?= htmlspecialchars($title ?? 'Accueil') ?></title>
 
     <!-- CSS commun -->
-    <link rel="stylesheet" href="Ressources/CSS/commun.css">
+    <link rel="stylesheet" href="Ressources/Css/commun.css">
     <!-- CSS header (menu profil) -->
-    <link rel="stylesheet" href="Ressources/CSS/header.css">
+    <link rel="stylesheet" href="Ressources/Css/header.css">
 
     <!-- CSS spécifique à la page -->
     <?php if (!empty($css)): ?>
-        <link rel="stylesheet" href="Ressources/CSS/<?= htmlspecialchars($css) ?>">
+        <link rel="stylesheet" href="Ressources/Css/<?= htmlspecialchars($css) ?>">
     <?php endif; ?>
     <?php if (!empty($js)): ?>
         <script src="Ressources/JVS/<?= htmlspecialchars($js) ?>" defer></script>
